87afdc025f8d57ad9085e0c6e8f41e8b181ff394
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-02 06:33:48 +0300

    addExpense func...

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,8 @@
 const expenses = [];
 let totalExpense = 0;
 
+function addExpense(description, amount) {
+    const expense = { id: expenses.length + 1, description, amount };
+    expenses.push(expense);
+    totalExpense += amount;
+}
